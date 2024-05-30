869240b5c1e6a29680bbd4a7c05ff9b95ba30a8f
derek2381 <satyakanthkarravula@gmail.com>
2024-05-30 21:38:00 +0530

    Today's Problem Solving Practicse

---
+++ CodeChef_Problems/CricketTorunament.cpp
@@ -0,0 +1,22 @@
+// problem link
+// https://www.codechef.com/problems/CRICKETUDYAM?tab=statement
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int a, b;
+	    cin >> a >> b;
+
+	   if(a-1 >= b){
+	       cout << "YES\n";
+	   }else{
+	       cout << "NO\n";
+	   }
+	}
+
+}
